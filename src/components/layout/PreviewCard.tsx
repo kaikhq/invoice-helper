c7ac71c20f9555bc3a87f515c1a5a0a16b956e02
Lawrence Lin <deduce@gmail.com>
2025-01-26 00:27:00 -0600

    test

--- src/components/layout/PreviewCard.tsx
+++ src/components/layout/PreviewCard.tsx
@@ -6,8 +6,8 @@ interface PreviewCardProps {
 
 export function PreviewCard({ children }: PreviewCardProps) {
   return (
-    <div className="sticky top-6">
-      <div className="bg-white/50 backdrop-blur-sm rounded-xl p-6 shadow-[0_4px_20px_rgba(0,0,0,0.04)] border border-white/80">
+    <div className="top-6">
+      <div className="">
         <h1>Hello card</h1>
         {children}
       </div>
